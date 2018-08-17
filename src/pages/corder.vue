68936ca6a4b22b6ba8d867554070ad2b2ed446d5
DiannaY <2421981682@qq.com>
2018-08-17 21:47:05 +0800

    first

---
+++ src/pages/corder.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+	  {{title}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+	  return {
+		  title : '用户定制页面——待写'
+	  }
+  }
+}
+</script>
+
---
+++ src/pages/crack.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+	  {{title}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+	  return {
+		  title : '碎屏宝商品页面-待写'
+	  }
+  }
+}
+</script>
